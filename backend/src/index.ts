62b24ce8fb93a1a626e78f26b64a05701c05ce49
Alex <malibaalex42@gmail.com>
2026-06-18 23:26:54 +0300

    ft(middleware): added validate a middleware that validate user input

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,14 +3,7 @@ import dotenv from "dotenv";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 
-import authRoutes from "@/routes/auth.routes.js";
-import writerRoutes from "@/routes/writer.js";
-import commentRoutes from "@/routes/comment.js";
-import postRoutes from "@/routes/post.js";
-import adminRoutes from "@/routes/admin.js";
-import userProfileRoutes from "@/routes/profile.js";
-import userRoutes from "@/routes/user.js";
-import landingPageRoutes from "@/routes/landing-page.js";
+import authRoutes from "@/routes/auth.js";
 
 dotenv.config();
 
@@ -27,16 +20,7 @@ app.use(cookieParser());
 
 app.use(express.json());
 
-app.use("/api/landing", landingPageRoutes);
-app.use("/api/auth", authRoutes);
-app.use("/api/admin", adminRoutes);
-app.use("/api/writer", writerRoutes);
-app.use("/api/user", userRoutes);
-app.use("/api/profile", userProfileRoutes);
-app.use("/api/post", postRoutes);
-app.use("/api/comment", commentRoutes);
-
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
   console.log(`Server listening on PORT: ${PORT}`);
