dcaaa4f6be75d718ea96ddf2e42572a820dfa5ec
Alex <malibaalex42@gmail.com>
2026-06-22 23:41:40 +0300

    ft(profile): added update profile and delete user in user settings

--- backend/src/routes/profile.ts
+++ backend/src/routes/profile.ts
@@ -1,10 +1,13 @@
+import { deleteUserController } from "@/controllers/auth/delete-user.js";
 import {
   getProfileService,
   updateProfileController,
 } from "@/controllers/profile.js";
 import { authenticate } from "@/middlewares/auth.js";
+import { requireRole } from "@/middlewares/role-require.js";
 import { validate } from "@/middlewares/validate.js";
 import { UpdateProfileSchema } from "@/schemas/profile.js";
+import { UserIdSchema } from "@/schemas/user.js";
 import express from "express";
 
 const router = express.Router();
@@ -16,9 +19,16 @@ router.use(authenticate);
 router.get("/profile", getProfileService);
 
 router.post(
-  "/",
+  "/profile/update",
   validate(UpdateProfileSchema, "body"),
   updateProfileController,
+);
+
+router.delete(
+  "/delete/:userId",
+  validate(UserIdSchema, "params"),
+  requireRole("user", "admin"),
+  deleteUserController,
 );
 
 export default router;
